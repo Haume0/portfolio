2c528d1d2f2c98c36ed74dfa12618d8a1e106545
Haume0 <haume341@outlook.com>
2026-01-07 20:49:56 +0300

    chore: scrolll smoothness

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -3,26 +3,26 @@ import { useEffect } from "react";
 import Lenis from "lenis";
 
 export default function SmoothScroll() {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      orientation: "vertical",
-      gestureOrientation: "vertical",
-      smoothWheel: true,
-    });
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 0.6,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            orientation: "vertical",
+            gestureOrientation: "vertical",
+            smoothWheel: true,
+        });
 
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
 
-    requestAnimationFrame(raf);
+        requestAnimationFrame(raf);
 
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
 
-  return null;
+    return null;
 }
