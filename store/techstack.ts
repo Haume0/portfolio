cc4b42f5e0c1504f1a9829921cd5d9247e1286b3
Haume0 <haume341@outlook.com>
2026-04-05 04:01:34 +0300

    Update techstack.ts

--- store/techstack.ts
+++ store/techstack.ts
@@ -1,61 +1,65 @@
 import { create } from "zustand";
 
 interface ITech {
-  id: string;
-  name: string;
+    id: string;
+    name: string;
 }
 interface ITechStore {
-  techs: ITech[];
-  tags: string[];
+    techs: ITech[];
+    tags: string[];
 }
 
 const useTechs = create<ITechStore>(() => ({
-  techs: [
-    {
-      id: "zed",
-      name: "Zed Editor",
-    },
-    {
-      id: "go",
-      name: "Go",
-    },
-    {
-      id: "react",
-      name: "React",
-    },
-    {
-      id: "next",
-      name: "Next",
-    },
-    {
-      id: "motion",
-      name: "Framer Motion",
-    },
-    {
-      id: "tailwind",
-      name: "Tailwind",
-    },
-    {
-      id: "typescript",
-      name: "TypeScript",
-    },
-    {
-      id: "figma",
-      name: "Figma",
-    },
-    {
-      id: "affinity",
-      name: "Affinity Studio",
-    },
-  ],
-  tags: [
-    "UI&UX Design",
-    "Graphic Design",
-    "Brand Identity",
-    "Full-Stack",
-    "Web Development",
-    "App Development",
-  ],
+    techs: [
+        {
+            id: "zed",
+            name: "Zed Editor",
+        },
+        {
+            id: "go",
+            name: "Go",
+        },
+        {
+            id: "typescript",
+            name: "TypeScript",
+        },
+        {
+            id: "next",
+            name: "Next",
+        },
+        {
+            id: "react",
+            name: "React",
+        },
+        {
+            id: "svelte",
+            name: "Svelte",
+        },
+        {
+            id: "motion",
+            name: "Framer Motion",
+        },
+        {
+            id: "tailwind",
+            name: "Tailwind",
+        },
+        {
+            id: "figma",
+            name: "Figma",
+        },
+        {
+            id: "affinity",
+            name: "Affinity Studio",
+        },
+    ],
+    tags: [
+        "UI&UX Design",
+        "Graphic Design",
+        "Brand Identity",
+        "Full-Stack",
+        "Web Development",
+        "App Development",
+    ],
 }));
 
 export default useTechs;
